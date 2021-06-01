2fb651617424c369bba42855dc828846b0582c8e
Thisath Thiyunajith <thisaththiyunajith@gmail.com>
2021-06-01 20:26:10 +0530

    Added new varibale and created the setter

--- src/java_oop_practise/Athlete.java
+++ src/java_oop_practise/Athlete.java
@@ -11,4 +11,23 @@ package java_oop_practise;
  */
 public class Athlete {
     
+    private String name;
+    private int speed;
+    private String marathonLocation;
+
+    
+    public Athlete(String name, int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+        
+    public void setmarathonLocation(String marathonLocation){
+        this.marathonLocation = marathonLocation;
+    }
+            
+    @Override
+    public String toString(){
+        return "Athlete "+name+" is running at "+speed+" mile per hour....." ;
+    }
+    
 }
